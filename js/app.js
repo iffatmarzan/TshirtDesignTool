d7a40cb26b0088e412e9a52264cb229b59562c2d
iffatmarzan <mouri331@yahoo.com>
2015-10-05 20:29:29 +0600

    'second'

--- js/app.js
+++ js/app.js
@@ -1,21 +1,4 @@
-//$(function () {
-//    var canvas = document.getElementById('FrontViewCanvas'),
-//        context = canvas.getContext('2d');
-//    make_base();
-//
-//
-//    function make_base() {
-//       // base_image = new Image();
-//      //  base_image.src = 'img/front.png';
-//       // base_image.onload = function () {
-//        //    context.drawImage(base_image, 0, 0);
-//       // }
-//    }
-//
-//    /**
-//     * Created by BS-92 on 10/5/2015.
-//     */
-//});
+
 var TShirtDesignTool =
 {
     init: function()
@@ -27,13 +10,13 @@ var TShirtDesignTool =
     {
         var canvas = new fabric.Canvas('FrontViewCanvas');
 
-        var hi = new fabric.Text('hello, world.', {
+        var SampleText = new fabric.Text('hello, world.', {
             left: canvas.getWidth() / 2,
             top: canvas.getHeight() / 2
         });
 
 
-        canvas.add(hi);
+        canvas.add(SampleText);
       //  canvas.setWidth($('#FrontViewDiv').width());
       //  canvas.setHeight($('#FrontViewDiv').height());
 
