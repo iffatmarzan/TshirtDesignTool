bffcacb681905a9bc4c29963487d08b67d733f92
mahbub-kuet <mrmanik.kuet@gmail.com>
2015-10-07 21:12:59 +0600

    Add text bug fix

--- js/app.js
+++ js/app.js
@@ -1,64 +1,38 @@
-
 var TShirtDesignTool =
 {
-    init: function()
-    {
+    init: function () {
         var _this = TShirtDesignTool;
-        var Text=$('#add-text-form textarea').val();
-        _this.drawText(Text);
+        _this.canvasInit();
     }
-    , drawText: function(Text)
-    {
-        Text==""?Text="dot dot dot":Text=Text;
-        var canvas = new fabric.Canvas('FrontViewCanvas');
-        canvas.remove();
-        var SampleText = new fabric.Text(Text, {
-            left: canvas.getWidth() / 2,
-            top: canvas.getHeight() / 2
-        });
-        canvas.add(SampleText);
-        var circle = new fabric.Circle({
-            radius: 20, fill: 'green', left: 100, top: 100
-        });
-        var triangle = new fabric.Triangle({
-            width: 20, height: 30, fill: 'blue', left: 50, top: 50
-        });
+    , canvasInit: function () {
+    window.canvas = new fabric.Canvas('FrontViewCanvas');
+}
+    , drawText: function (Text) {
 
-        canvas.add(circle, triangle);
-
-        //SampleText = new fabric.Text(Text+"changed", {
-        //    left: canvas.getWidth() / 2,
-        //    top: canvas.getHeight() / 2
-        //});
-        //canvas.add(SampleText);
-
-
-        canvas.renderAll();
-      //  canvas.setWidth($('#FrontViewDiv').width());
-      //  canvas.setHeight($('#FrontViewDiv').height());
-
+    if (window.canvas.getObjects().length > 0) {
+        window.canvas.item(0).text = Text;
+        window.canvas.renderAll();
+        return;
     }
+
+    var SampleText = new fabric.Text(Text, {
+        left: canvas.getWidth() / 2,
+        top: canvas.getHeight() / 2
+    });
+
+    window.canvas.add(SampleText);
+    return;
+
+}
 };
 
-$(document).ready(function()
-{
-   TShirtDesignTool.init();
-    $('#add-text-form a').bind("click",function(){
-        var Text=$('#add-text-form textarea').val();
-        //TShirtDesignTool.drawText(Text);
+$(document).ready(function () {
+    TShirtDesignTool.init();
 
-        var canvas = new fabric.Canvas('FrontViewCanvas');
-        canvas.remove();
+    $('#add-text-form a').bind("click", function () {
 
-        var SampleText = new fabric.Text(Text, {
-            left: canvas.getWidth() / 2,
-            top: canvas.getHeight() / 2
-        });
-        canvas.add(SampleText);
-        var triangle = new fabric.Triangle({
-            width: 20, height: 30, fill: 'red', left: 50, top: 50
-        });
+        var Text = $('#add-text-form textarea').val();
+        TShirtDesignTool.drawText(Text);
 
-        canvas.add(triangle);
     });
 });
