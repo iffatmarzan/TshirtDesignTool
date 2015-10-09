12899b2b7c1bb08bdd18ffaa30c3978f8c0c9daa
mahbub-kuet <mrmanik.kuet@gmail.com>
2015-10-09 15:31:49 +0600

    Modification of Editor panel

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var TShirtDesignTool =
     init: function () {
         var _this = TShirtDesignTool;
         _this.canvasInit();
+       // _this.drawText("Hello world");
+
         //window.canvas.on('mouse:down', function(options) {
         //    console.log(options.e.clientX, options.e.clientY);
         //    if (options.target) {
@@ -18,52 +20,85 @@ var TShirtDesignTool =
     //canvas.isDrawingMode=true;
     canvas.renderAll();
 }
-    , drawText: function (Text) {
-    if (window.canvas.getObjects().length > 0) {
-        window.canvas.item(0).text = Text;
-        window.canvas.renderAll();
+    , drawText: function () {
+
+    //if (window.canvas.getObjects().length > 0) {
+    //    window.canvas.item(0).text = Text;
+    //    window.canvas.renderAll();
+    //    return;
+    //}
+
+    var Text = $('#add-text-form textarea').val();
+    if(Text==="")
         return;
-    }
     var SampleText = new fabric.Text(Text, {
         left: canvas.getWidth() / 2,
         top: canvas.getHeight() / 2
     });
 
     SampleText.on('selected', function() {
-        console.log('Text is selected');
+       // console.log('Text is selected');
+        TShirtDesignTool.changeToEditorPanel(SampleText.text);
     });
 
     SampleText.on('added', function() {
         console.log('Text added');
     });
 
+    SampleText.on('onkeydown', function(e) {
+       // not fired
+    });
+
     window.canvas.add(SampleText);
+    TShirtDesignTool.changeToEditorPanel(Text);
     return;
 } // end of drawText
-    ,changeTextPanel:function(){
+    ,changeToEditorPanel:function(Text){
     $('#textPanel').empty();
     $('#textPanel').css("border","1px solid darkgray");
       var editorPanel=' <h6>Text Properties:</h6>'+
-             '<textarea name=""></textarea>'+
+             '<textarea name="" onkeyup="return TShirtDesignTool.updateText(this.value)">'+Text+'</textarea>'+
              '<select name="font">'+
              '<option value="" selected>Change font</option><option value="1">OldSansBlack</option>'+
-              '<option value="2">Megazine</option><option value="3" >Typodermic</option><option value="4">Impact</option>'+
+             '<option value="2">Megazine</option><option value="3" >Typodermic</option><option value="4">Impact</option>'+
             '</select>';
     $('#textPanel').append(editorPanel);
+}   //end of changeToEditorPanel
+    ,changeToAddTextPanel: function(){
+        $('#textPanel').empty();
+        $('#textPanel').removeAttr('style');
+        var addTextPanel='<div class="panel">'+
+            '<h5>Add Text</h5>'+
+            '<form id="add-text-form" class="content">'+
+            '<textarea class="text full input-text-1" name=""></textarea>'+
+            '<fieldset>'+
+            '<a href="#" title="Add" onclick="return TShirtDesignTool.drawText()" class="primary-button full">Add Text</a>'+
+            '</fieldset>'+
+            '</form>'+
+            '</div>';
+        $('#textPanel').append(addTextPanel);
 }
+    ,updateText: function(e){
+    //var activeObject=window.canvas.getActiveObject();
+    //activeObject.setAttribute('text',this.value);
+    window.canvas.getActiveObject().text=e;
+    canvas.renderAll();
+}
+
+
 };
 
 $(document).ready(function () {
     TShirtDesignTool.init();
 
-    $('#add-text-form a').bind("click", function () {
+    document.onkeydown = function(e) {
+        if (46 === e.keyCode || e.keyCode===110) {
+            var activeObject=window.canvas.getActiveObject();
+            if(activeObject!==null){
+                window.canvas.remove(activeObject);
+            }
+            window.canvas.renderAll();
+        }};
 
-        var Text = $('#add-text-form textarea').val();
-        if(Text!=""){
-            TShirtDesignTool.drawText(Text);
-            TShirtDesignTool.changeTextPanel();
-        }
 
-
-    });
 });
