2a153d3181abf801e135f4f63d7f002d6bf1397a
Mahbubur Rahman <mrmanik.kuet@gmail.com>
2015-10-11 18:29:17 +0600

    CurveText added

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ var TShirtDesignTool =
     var Text = $('#add-text-form textarea').val();
     if(Text==="")
         return;
-    var SampleText = new fabric.Text(Text, {
+    var SampleText = new fabric.CurvedText(Text, {
         left: canvas.getWidth() / 2,
         top: canvas.getHeight() / 2,
         //left: 380,
@@ -72,21 +72,26 @@ var TShirtDesignTool =
     });
 
     window.canvas.add(SampleText);
-    TShirtDesignTool.changeToEditorPanel(Text);
+    canvas.renderAll();
+    TShirtDesignTool.changeToEditorPanel();
     return;
 }                                                       // end of drawText
     ,changeToEditorPanel:function(){
-    var selectedObjecct=window.canvas.getActiveObject();
+    var selectedObject=window.canvas.getActiveObject();
+    if(!selectedObject){
+        selectedObject={'text':$('#textPanel textarea').val(),
+                         'fill':'#000'}
+    }
     $('#textPanel').empty();
     $('#textPanel').css("border","1px solid darkgray");
       var editorPanel=' <h6>Text Properties:</h6>'+
-             '<textarea name="" onkeyup="return TShirtDesignTool.updateText(this.value)">'+selectedObjecct.text+'</textarea>'+
+             '<textarea name="" onkeyup="return TShirtDesignTool.updateText(this.value)">'+selectedObject.text+'</textarea>'+
              '<select name="font" onchange="return TShirtDesignTool.setTextFont(this.value)">'+
              '<option value="sans-serif" selected>sans-serif</option><option value="OldSansBlack">OldSansBlack</option>'+
              '<option value="Megazine">Megazine</option><option value="monospace" >monospace</option><option value="Impact">Impact</option>'+
              '</select>'+
              '<b>Spacing: </b><input type="number"  min="1" max="10" value="1" onchange="return TShirtDesignTool.setTextSpacing(this.value)" >'+
-             '<br/><b>Color: </b><input type="color" onchange="return TShirtDesignTool.setTextColor(this.value)" value='+selectedObjecct.fill+'>'+
+             '<br/><b>Color: </b><input type="color" onchange="return TShirtDesignTool.setTextColor(this.value)" value='+selectedObject.fill+'>'+
              '<br/><b>Arc :</b><br/> <input type="range"  min="-50" max="50" value="0" onchange="return TShirtDesignTool.setTextArc(this.value)"/><br>';
 
     $('#textPanel').append(editorPanel);
@@ -114,7 +119,8 @@ var TShirtDesignTool =
     window.canvas.renderAll();
 }
     ,setTextSpacing:function(e){
-
+    window.canvas.getActiveObject().set('spacing',e);
+    canvas.renderAll();
 }
     ,setTextColor:function(e){
          window.canvas.getActiveObject().fill=e;
@@ -122,7 +128,8 @@ var TShirtDesignTool =
 
 }
     ,setTextArc:function(e){
-
+    window.canvas.getActiveObject().set('radius',e);
+    canvas.renderAll();
 }
 
 };                 //end of TShirtDesignTool
