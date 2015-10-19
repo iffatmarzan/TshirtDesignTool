b33c5e50eb41cc376227a427526d600cc24ac9d0
Afifa Tahira <afifa.tahira@bs-23.com>
2015-10-19 18:41:15 +0600

    Text spacing for normal text.

--- js/app.js
+++ js/app.js
@@ -73,17 +73,19 @@ var TShirtDesignTool =
                 //left: 380,
                 //top:370,
                 fontFamily: 'sans-serif',
-                fontSize:30
+                fontSize:30,
+                letterSpacing: 8
             });
             var _canvas=window.canvas;
             var curveText= new fabric.CurvedText(Text,{
-                textAlign: 'center',
+                textAlign: 'left',
                 fontFamily: 'sans-serif'
             });
             var left=_canvas.getWidth() / 2- curveText.getWidth()/2;
             var top=_canvas.getHeight() / 2- curveText.getHeight();
             curveText.set({'left':left,
-                            'top':top
+                            'top':top,
+                letterSpacing: 10
                         });
 
             curveText.on('selected', function() {
@@ -95,7 +97,7 @@ var TShirtDesignTool =
     });
 
             window.canvas.add(curveText);
-            //window.canvas.add(SampleText);
+            window.canvas.add(SampleText);
             canvas.renderAll();
             TShirtDesignTool.changeToEditorPanel();
             return;
@@ -112,6 +114,7 @@ var TShirtDesignTool =
             if(!selectedObject){
                 selectedObject=window.canvas.item(0);
             }
+    console.log(selectedObject.get('letterSpacing'));
             TShirtDesignTool.showPanel('#editor-panel');
 
 
@@ -155,6 +158,7 @@ var TShirtDesignTool =
             var activeObject=window.canvas.getActiveObject();
             if(!activeObject)
                 activeObject=window.canvas.item(0);
+            //activeObject.set('spacing', parseInt(e));
 
             if(activeObject.effect=='arc')
                 activeObject.set({
@@ -303,5 +307,87 @@ $(document).ready(function () {
 
     $(document).foundation();
 
+
+});
+
+fabric.util.object.extend(fabric.Text.prototype, {
+    _renderChars: function(method, ctx, chars, left, top) {
+        var characters = String.prototype.split.call(chars, ''),
+            index = 0,
+            current,
+            currentPosition = left,
+            y = top,
+            align = 1,
+            ls = 0;
+
+        if (this.letterSpacing !== undefined)
+            ls = this.letterSpacing;
+
+        if (this.textAlign === 'right') {
+            characters = characters.reverse();
+            align = -1;
+        } else if (this.textAlign === 'center') {
+            var totalWidth = 0;
+            for (var i = 0; i < characters.length; i++) {
+                totalWidth += (ctx.measureText(characters[i]).width + ls);
+            }
+            //currentPosition = x - (totalWidth / 2); //original
+            currentPosition = left - (totalWidth / 2); //just randomly replaced x with 'left' to avoid error, should be taken care of before finalizing
+        }
+
+        while (index < chars.length) {
+            current = characters[index++];
+            ctx.fillText(current, currentPosition, y);
+            currentPosition += (align * (ctx.measureText(current).width + ls));
+        }
+    },
+
+})
+
+fabric.util.object.extend(fabric.IText.prototype, {
+    letterSpacing : 0,
+    _getTextWidth: function(ctx, textLines) {
+        var maxWidth = ctx.measureText(textLines[0] || '|').width;
+        var maxLineLength = textLines[0].length;
+        for (var i = 1, len = textLines.length; i < len; i++) {
+            var currentLineWidth = ctx.measureText(textLines[i]).width;
+            if (currentLineWidth > maxWidth) {
+                maxWidth = currentLineWidth;
+                maxLineLength = textLines[i].length;
+            }
+        }
+
+        return maxWidth + (this.letterSpacing * maxLineLength);
+    },
+    _getWidthOfChar: function(ctx, _char, lineIndex, charIndex) {
+        if (this.textAlign === 'justify' && /\s/.test(_char)) {
+            return this._getWidthOfSpace(ctx, lineIndex);
+        }
+
+        var styleDeclaration = this._getStyleDeclaration(lineIndex, charIndex);
+        this._applyFontStyles(styleDeclaration);
+        var cacheProp = this._getCacheProp(_char, styleDeclaration);
+
+        if (this._charWidthsCache[cacheProp] && this.caching) {
+            return this._charWidthsCache[cacheProp] + this.letterSpacing;
+        }
+        else if (ctx) {
+            ctx.save();
+            var width = this._applyCharStylesGetWidth(ctx, _char, lineIndex, charIndex);
+            ctx.restore();
+            return width + this.letterSpacing;
+        }
+    },
+    _getWidthOfSpace: function (ctx, lineIndex) {
+        var lines = this.text.split(this._reNewline),
+            line = lines[lineIndex],
+            words = line.split(/\s+/),
+            wordsWidth = this._getWidthOfWords(ctx, line, lineIndex),
+            widthDiff = this.width - wordsWidth,
+            numSpaces = words.length - 1,
+            width = widthDiff / numSpaces;
+
+        return width + this.letterSpacing;
+    }
 
 });
