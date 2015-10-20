e6ee62841f20fa3218180ca7c1e7fffe638a8a03
Afifa Tahira <afifa.tahira@bs-23.com>
2015-10-20 19:21:06 +0600

    Remove button added by extending fabric classes.

--- js/text.classExtend.js
+++ js/text.classExtend.js
@@ -109,3 +109,148 @@ fabric.util.object.extend(fabric.IText.prototype, {
     }
 
 });
+
+fabric.util.object.extend(fabric.Object.prototype, {
+
+    _drawControl: function(control, ctx, methodName, left, top) {
+         if (!this.isControlVisible(control)) {
+            return;
+         }
+         var size = this.cornerSize;
+         isVML() || this.transparentCorners || ctx.clearRect(left, top, size, size);
+
+        if(control !== 'bl')      //MODIFICATION BY AFIFA only if condition, not the body
+            ctx[methodName](left, top, size, size);
+
+         //MODIFICATION BY AFIFA starts
+
+        var SelectedIconImage = new Image();
+        if(control === 'bl') {
+             SelectedIconImage.src = 'img/cross.png';
+             ctx.drawImage(SelectedIconImage, left, top, size, size);
+     }
+         //MODIFICATION BY AFIFA ends
+    }
+
+});
+
+fabric.util.object.extend(fabric.Canvas.prototype, {
+    //MODIFICATION BY AFIFA Starts
+    /**
+     * Cursor value used for cross point
+     * @type String
+     * @default
+     */
+    pointerCursor:         'pointer',
+    // MODIFICATION BY AFIFA ends
+
+    _getActionFromCorner: function(target, corner) {
+        var action = 'drag';
+        if (corner) {
+            action = (corner === 'ml' || corner === 'mr')
+                ? 'scaleX'
+                : (corner === 'mt' || corner === 'mb')
+                ? 'scaleY'
+                : corner === 'mtr'
+                ? 'rotate'
+                : corner === 'bl' //MODIFICATION BY AFIFA
+                ? 'remove' //MODIFICATION BY AFIFA
+                : 'scale';
+        }
+        return action;
+    },
+
+     _setupCurrentTransform: function (e, target) {
+         if (!target) {
+             return;
+         }
+
+         var pointer = this.getPointer(e),
+             corner = target._findTargetCorner(this.getPointer(e, true)),
+             action = this._getActionFromCorner(target, corner),
+             origin = this._getOriginFromCorner(target, corner);
+
+         //MODIFICATION BY AFIFA starts
+
+         if (action == 'remove')
+         {
+             if (this.getActiveGroup() && this.getActiveGroup()!= 'undefined')
+             {
+                 this.getActiveGroup().forEachObject(function(o)
+                 {
+                     o.remove();
+                 });
+                 this.discardActiveGroup();
+             }
+             else
+             {
+                 target.remove();
+             }
+             return;
+         }
+
+         //MODIFICATION BY AFIFA ends
+
+         this._currentTransform = {
+             target: target,
+             action: action,
+             scaleX: target.scaleX,
+             scaleY: target.scaleY,
+             offsetX: pointer.x - target.left,
+             offsetY: pointer.y - target.top,
+             originX: origin.x,
+             originY: origin.y,
+             ex: pointer.x,
+             ey: pointer.y,
+             left: target.left,
+             top: target.top,
+             theta: degreesToRadians(target.angle),
+             width: target.width * target.scaleX,
+             mouseXSign: 1,
+             mouseYSign: 1
+         };
+
+         this._currentTransform.original = {
+             left: target.left,
+             top: target.top,
+             scaleX: target.scaleX,
+             scaleY: target.scaleY,
+             originX: origin.x,
+             originY: origin.y
+         };
+
+         this._resetCurrentTransform(e);
+     },
+
+    _setCornerCursor: function(corner, target) {
+         if (corner === 'bl') {               //MODIFICATION BY AFIFA
+             this.setCursor(this.pointerCursor);  //MODIFICATION BY AFIFA
+         }                                             //MODIFICATION BY AFIFA
+         else if (corner in cursorOffset) {              //MODIFICATION BY AFIFA ONLY ELSE
+             this.setCursor(this._getRotatedCornerCursor(corner, target));
+         }
+         else if (corner === 'mtr' && target.hasRotatingPoint) {
+             this.setCursor(this.rotationCursor);
+         }
+         else {
+             this.setCursor(this.defaultCursor);
+             return false;
+         }
+     }
+
+ });
+
+var cursorOffset = {
+    mt: 0, // n
+    tr: 1, // ne
+    mr: 2, // e
+    br: 3, // se
+    mb: 4, // s
+    bl: 5, // sw
+    ml: 6, // w
+    tl: 7 // nw
+};
+
+isVML = function() { return typeof G_vmlCanvasManager !== 'undefined'; };
+
+degreesToRadians = fabric.util.degreesToRadians;
